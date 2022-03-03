54430b4de186e842706e6dbd45b700d680174caf
Devkant <devkant21@gmail.com>
2022-03-03 19:47:02 +0530

    Add useEffect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import './App.css';
+import Effect from './Effect';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Effect />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Effect.js
@@ -0,0 +1,17 @@
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Effect () {
+    const [data, setData] = useState("");
+
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+        .then((response) => {
+            setData(response.data[0].email);
+            console.log("API hello")
+        });
+    },[]);
+    return <div>Hi {data}</div>;
+}
+export default Effect;
